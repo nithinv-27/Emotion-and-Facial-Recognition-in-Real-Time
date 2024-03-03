630b7bbbc49c2dedb35b67f9300ef2569393abd6
Taraka Nithin Vankala <nithintaraka.v@gmail.com>
2024-03-03 23:22:10 +0530

    modified test file to run torch model

--- test_file.py
+++ test_file.py
@@ -1,9 +1,28 @@
 import cv2
 import numpy as np
+import torch
+from torchvision import models
 from keras.models import load_model
+import torch.nn as nn
+import os
+
+data_dir='./face_recognition'
+img_size=[48,48]
+batch_size=128
+
+num_class=len(os.listdir(data_dir))
+classes= os.listdir(data_dir)
 
 # Load the trained face detection model
-model1 = load_model('face_detection_model.h5')  # Load your trained face detection model here
+model1_weights = torch.load('resnet_face_detection_model.pth', weights_only=True)
+print(type(model1_weights))
+model1 = models.resnet18()
+num_ftrs = model1.fc.in_features  #num_ftrs=512
+model1.fc=nn.Sequential(
+    nn.Linear(num_ftrs, num_class),
+    nn.Softmax(dim=1)
+) #modifying fully connected layer to classify num_classes
+model1.load_state_dict(torch.load('resnet_face_detection_model.pth')) # Load your trained face detection model here
 model2 = load_model('vgg16emotion_detection_model.h5')  # Load your trained emotion detection model here
 
 # Define the emotion labels
@@ -38,15 +57,20 @@ while True:
         face_region = np.expand_dims(face_region, axis=0)  # Add batch dimension
         face_region = face_region / 255.0  # Normalize the face region
 
-        face_region_f = frame_gray[y:y + h, x:x + w]
+        face_region_f = frame[y:y + h, x:x + w]
         face_region_f = cv2.resize(face_region_f, (48, 48))
         face_region_f = np.expand_dims(face_region_f, axis=0)  # Add batch dimension
         face_region_f = face_region_f / 255.0  # Normalize the face region
 
         # Make predictions using the emotion detection model
-        predictions1=model1.predict(face_region_f)
+        # print("Type of model1, model2", type(model1), type(model2))
+        face_region_f= torch.from_numpy(face_region_f)
+        face_region_f= face_region_f.permute(0,3,1,2)
+        # print("face_region_f shape= ", face_region_f.shape)
+        predictions1=model1(face_region_f.float())
         predictions2 = model2.predict(face_region)
         predicted_emotion = emotion_labels[np.argmax(predictions2)]
+        predictions1= predictions1.detach().numpy()
         predicted_face=face_labels[np.argmax(predictions1)]
 
         # Display the emotion text on the frame
